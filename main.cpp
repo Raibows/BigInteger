2f448f20b675c92790da4dbfe05057c4a4493b4d
raibows <raibows@hotmail.com>
2019-12-29 14:22:01 +0800

    update assign greater equal

--- main.cpp
+++ main.cpp
@@ -18,28 +18,64 @@ private:
     vector<int> value;
     int real_length;
     int sign;
+    void set_number(string value, bool positive_sign);
+    bool greater(BigInt b);
 public:
     BigInt(int length);
-    BigInt(string value, bool positive_sign);
+    BigInt(string value_and_sign);
     BigInt(long long int value);
     string to_string();
+    BigInt absolute();
+    bool get_positive_sign();
+    int get_real_length();
+    void operator = (BigInt b);
+    bool operator == (BigInt b);
+    bool operator != (BigInt b);
+    bool operator > (BigInt b);
+    bool operator >= (BigInt b);
+    bool operator < (BigInt b);
+    bool operator <= (BigInt b);
 };
 
-BigInt::BigInt(int length) {
-    this->value = vector<int>(length, 0);
-    this->real_length = 0;
-    this->sign = 0;
+bool BigInt::get_positive_sign() {
+    return this->sign == 0 ? true : false;
 }
 
-BigInt::BigInt(string value, bool positive_sign) {
+int BigInt::get_real_length() {
+    return this->real_length;
+}
+
+
+void BigInt::set_number(string value, bool positive_sign) {
     this->value = vector<int>(value.size(), 0);
-    this->real_length = value.size();
     this->sign = positive_sign ? 0 : 1;
     this->value[0] = sign;
     int i = 0;
     for (string::iterator iter = value.end()-1; iter != value.begin()-1; --iter) {
         this->value[++i] = (*iter - '0');
     }
+    this->real_length = i;
+}
+
+BigInt::BigInt(int length) {
+    this->value = vector<int>(length+1, 0);
+    this->real_length = 0;
+    this->sign = 0;
+}
+
+BigInt::BigInt(string value_and_sign) {
+    bool positive;
+    if (value_and_sign[0] == '-')
+    {
+        positive = false;
+        value_and_sign = value_and_sign.substr(1);
+    } else if (value_and_sign[0] == '+') {
+        positive = true;
+        value_and_sign = value_and_sign.substr(1);
+    } else {
+        positive = true;
+    }
+    this->set_number(value_and_sign, positive);
 }
 
 BigInt::BigInt(long long int value) {
@@ -68,10 +104,44 @@ string BigInt::to_string() {
     return temp;
 }
 
+void BigInt::operator=(BigInt b) {
+    bool positive = b.sign == 0 ? true : false;
+    string value = b.to_string();
+    if (positive == false)
+    {
+        value = value.substr(1);
+    }
+    this->set_number(value, positive);
+}
+
+bool BigInt::operator==(BigInt b) {
+    return this->to_string() == b.to_string();
+}
+
+bool BigInt::greater(BigInt b) {
+    if (this->sign != b.sign){
+        return this->sign == 0 ? true : false;
+    }
+    if (this->real_length != b.real_length){
+        return this->real_length > b.real_length;
+    }
+    vector<int>::iterator iter1 = this->value.end()-1;
+    vector<int>::iterator iter2 = b.value.end()-1;
+    while (iter1 >= this->value.begin() && iter2 >= b.value.begin()){
+        if (*iter1 != *iter2)
+        {
+            return *iter1 > *iter2;
+        }
+    }
+    return false;
+}
+
 int main(int argc, char *argv[]) {
-    string value = "1234567891000002132131230";
-    BigInt x(value, false);
-    cout << "hello" << endl;
+    ios::sync_with_stdio(false);
+    string value = "+1234567891000002132131230";
+    BigInt x(value);
+    cout << value.size() << endl;
     cout << x.to_string() << endl;
+    cout << x.get_positive_sign() << endl << x.get_real_length() <<endl;
     return 0;
 }
